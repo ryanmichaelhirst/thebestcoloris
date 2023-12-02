42db84f4f4b9c52a1160471b1fe23809f014b6ee
RyanMichael Hirst <ryanmichaelhirst@gmail.com>
2023-12-01 23:25:21 -0500

    Remove /public and /build

--- public/build/_shared/remix_hmr-FCZ6TBOW.js
+++
@@ -1,9 +0,0 @@
-import {
-  createHotContext
-} from "/build/_shared/chunk-XMMNNE6D.js";
-import "/build/_shared/chunk-K6PKGSTD.js";
-import "/build/_shared/chunk-PNG5AS42.js";
-export {
-  createHotContext
-};
-//# sourceMappingURL=/build/_shared/remix_hmr-FCZ6TBOW.js.map
